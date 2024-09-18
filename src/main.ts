b1b801333ec03f4878cb69afc4dabc071eb8cef4
guojianli <1547702880@qq.com>
2024-09-18 17:21:04 +0800

    feat: 异常filter

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 import { UnloginFilter } from './unlogin.filter';
+import { CustomExceptionFilter } from './custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   app.useGlobalInterceptors(new InvokeRecordInterceptor());
   app.useGlobalFilters(new UnloginFilter());
+  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(configService.get('nest_server_port') || 3000);
 }
 bootstrap();
