34a50a0e5129426d20e0e8474dd393a8285f6564
guojianli <1547702880@qq.com>
2024-09-18 15:35:50 +0800

    feat: 添加响应拦截器

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(configService.get('nest_server_port') || 3000);
 }
 bootstrap();
